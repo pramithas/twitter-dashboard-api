e6503acd83afbe375e3ffb0335d9409581082615
pramithas <dhakalpramithas@gmail.com>
2023-07-22 20:32:31 +0545

    Initial commit

---
+++ src/main/java/com/pramithas/twitterdashboard/TwitterDashboardApplication.java
@@ -0,0 +1,11 @@
+package com.pramithas.twitterdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterDashboardApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterDashboardApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pramithas/twitterdashboard/TwitterDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramithas.twitterdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
